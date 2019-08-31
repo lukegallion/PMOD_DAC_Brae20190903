1c28de37ab07c1f8f275e0a47b835421cc49a728
brae-pete <bvp391@gmail.com>
2019-08-31 19:09:09 -0400

    PMOD Classes + Example + Python Controller

--- ExampleScript.py
+++ ExampleScript.py
@@ -10,10 +10,22 @@ FILENAME = "SavedData.csv"
 # Create a Bertran High Voltage Controller object
 bert = bertran.BertranControl()
 
-start_time = time.time()
+# Initialize electrodes + ADC channels (This could probably be a function, since it won't change much
+bert.set_voltage_channel_adc(1, 5)
+bert.set_voltage_channel_adc(2, 2)
+bert.set_current_channel_adc(1, 3)
+bert.set_current_channel_adc(2, 4)
+bert.set_voltage_channel(3, 1)
+bert.set_voltage_channel(3, 0)
 
 # Create new dataframe
 run_data = bertran.create_electropherogram_dataframe()
+
+# Set voltages and start voltage
+bert.set_voltage(1, 425)
+bert.set_voltage(2, 312)
+start_time = time.time()
+bert.load_changes()
 
 while time.time() - start_time < RUN_TIME:
     # Retreive new data
@@ -23,6 +35,7 @@ while time.time() - start_time < RUN_TIME:
     # Sleep for a little bit
     time.sleep(1 / SAMPLE_FREQ)
 
+bert.power_down()
 
-sns.lineplot(x='Time', y = ['C1_voltage', 'C1_current'])
+sns.lineplot(x='Time', y=['C1_voltage', 'C1_current'])
 run_data.to_csv(FILENAME)
